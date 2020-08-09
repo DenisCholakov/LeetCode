d71909f9ee1853ce304d422ade7ec336d4ffdc11
DenisCholakov <denisscholakov@gmail.com>
2020-08-09 15:40:43 +0300

    PalindromeNumber

---
+++ PalindromeNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PalindromeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool bl = IsPalindrome(int.Parse(Console.ReadLine()));
+        }
+
+        public static bool IsPalindrome(int x)
+        {
+            int reversed = 0;
+            int temp = x;
+
+            if (x < 0)
+            {
+                return false;
+            }
+
+            while (temp != 0)
+            {
+                reversed = reversed * 10 + temp % 10;
+                temp /= 10;
+            }
+
+            return reversed == x;
+        }
+    }
+}
